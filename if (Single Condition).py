cc2cc3b264219a3ee396167aa28b205f6fc69ac4
1996amann-blip <1996amann@gmail.com>
2026-06-18 14:31:20 +0530

    Add files via upload

---
+++ if (Single Condition).py
@@ -0,0 +1,47 @@
+"""
+#1
+a=float(input("Enter a number : "))
+if a>0:
+    print("Positive")
+
+#2
+Age=int(input("Enter your age : "))
+if Age>=18:
+    print("Eligible to vote.")
+
+#3
+a=int(input("Enter a number : "))
+if a%7==0:
+    print(a," is divisible by 7")
+
+#4
+m=float(input("Enter your marks : "))
+if m>40:
+    print("Pass")
+
+#5
+n=float(input("Enter a number : "))
+if n>100:
+    print(n," is greater than 100")
+
+#6
+temp=float(input("Enter the temprature in Celcius : "))
+
+if temp>45:
+    print("Temprature has exceeded 45"+chr(176)+"C.")
+
+#8
+p=(input("Enter the password : "))
+if p=='admin123':
+    print("Logged In")
+
+#9
+n=float(input("Enter a number : "))
+if n%10==0:
+    print(n," is a multiple of 10")
+
+#10
+b=float(input("Enter the Balance : "))
+if b<500: #500 is minimum limit
+    print("WARNING: Balance is below minimum limit.")
+"""
